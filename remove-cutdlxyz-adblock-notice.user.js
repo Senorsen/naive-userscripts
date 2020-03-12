c34f605102a16614e1f565199ef9a7bd1ac09418
Zhang Sen <senorsen.zhang@gmail.com>
2020-03-12 15:20:34 +0800

    Create remove-cutdlxyz-adblock-notice.user.js

---
+++ remove-cutdlxyz-adblock-notice.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Remove CutDl.Xyz AdBlock info
+// @namespace    https://senorsen.com
+// @version      0.1
+// @description  Remove CutDl.Xyz AdBlock info
+// @author       Senorsen
+// @match        https://cutdl.xyz/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.checkAdblockUser = ''
+})();
